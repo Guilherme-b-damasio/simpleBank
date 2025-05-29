996c93d2d93158564865d7114e83a4df97d1c870
guilherme <guilherme-b-damasio@estudante.sesisenai.org.br>
2025-05-28 22:44:24 -0300

    feat: add id_region in test of new users

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -23,6 +23,7 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'id_region' => '111.111.111-11',
         ]);
 
         $this->assertAuthenticated();
