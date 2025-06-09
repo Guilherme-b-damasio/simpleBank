7514c97c650adad2a1cf4de3aa91f87660c2fba4
guilherme <guilherme-b-damasio@estudante.sesisenai.org.br>
2025-06-09 08:50:04 -0300

    feat: Add create account in store user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Account;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -43,6 +44,12 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'id_region' => $request->id_region,
+        ]);
+
+        $account = Account::create([
+            'balance' => 100.000,
+            'credit_limit' => 100.000,
+            'user_id' => $user->id
         ]);
 
         event(new Registered($user));
